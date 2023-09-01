a67019c4ada96242b0769c88728af4b95be9a000
zimmra <paytonz@icloud.com>
2023-09-01 15:03:31 -0700

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,10 +2,10 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Zimmras Kasm Registry',
+    description: 'My Repo for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://zimmra.github.io/kasm-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
